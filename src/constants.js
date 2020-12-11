aa7c56a56589f9262f86ef4a174fde90e49d7428
rxm <383902558@qq.com>
2020-12-11 15:53:16 +0800

    fix: add error message

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
     ERRORS: {
         INVALID_USE: '本工具只适用于segma-ui',
+        GIT_IS_CHANGED: '本地仓库存在修改，请先暂存或提交',
         WRONG_ICON_PATH: 'icon目录不存在',
         CONFIG_NOT_EXIST: '缺少配置文件icon-update.json，已自动生成模板',
         INVALID_CONFIG: '无效的配置文件',
